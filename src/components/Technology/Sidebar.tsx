785f5b1dc80ac2ffdeaceccd2612f20e80f6ef18
Maqsuda Binte Mahboob <maqsudamahboob@gmail.com>
2026-09-22 18:51:17 +0600

    modify technologies card

--- src/components/Technology/Sidebar.tsx
+++ src/components/Technology/Sidebar.tsx
@@ -15,31 +15,38 @@ const Sidebar = ({
   console.log(techs);
   return (
     <div className="border border-gray-100 p-5 rounded-lg shadow-xs">
-      <h2>Your Stack</h2>
+      <h2 className="font-bold">Your Stack</h2>
       <p>
         {techs.length === 0 ? (
-          "No technologies selected yet"
+          <p className="text-gray-300">No technologies selected yet</p>
         ) : (
-          <>
+          <p className="text-gray-300">
             {techs.length} {techs.length === 1 ? "Technology" : "Technologies"}{" "}
             Saved{" "}
-          </>
+          </p>
         )}
       </p>
       {techs.length === 0 ? (
-        <p className="flex justify-center items-center border border-gray-100 mt-3 rounded-xl p-5">
+        <p className="flex justify-center items-center border text-gray-300 border-gray-100 mt-3 rounded-xl p-5">
           Your Stack is empty.
         </p>
       ) : (
         <>
           {techs.map((tech) => (
             <div className="flex justify-between items-center border border-gray-100 mt-1 rounded-xl p-2">
-              <img className="w-10 h-10" src={tech.icon} />
-              <h2>{tech.category}</h2>
-              <ImCross
-                className="size-3 text-gray-400 font-medium"
-                onClick={() => handleRemoveTech(tech.id)}
-              />
+              <div className="flex justify-baseline gap-2 items-center">
+                <img className="w-10 h-10" src={tech.icon} />
+                <div>
+                  <p className="font-bold">{tech.name}</p>
+                  <p className="text-gray-300">{tech.category}</p>
+                </div>
+              </div>
+              <div>
+                <ImCross
+                  className="size-3 text-gray-400 font-medium"
+                  onClick={() => handleRemoveTech(tech.id)}
+                />
+              </div>
             </div>
           ))}
         </>
